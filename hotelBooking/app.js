b058b63aa67b9978588fbab57638e9532ae55de6
costariann <afuaoye30@gmail.com>
2025-09-02 10:44:42 +0000

    render psql setup for backend deployment

--- hotelBooking/app.js
+++ hotelBooking/app.js
@@ -66,14 +66,7 @@ const upload = multer({
 
 // Postgres connection pool with SSL
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 20000,
+  connectionString: process.env.DATABASE_URL,
   ssl:
     process.env.DB_HOST !== 'localhost' ? { rejectUnauthorized: false } : false,
 });
